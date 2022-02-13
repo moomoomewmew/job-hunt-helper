bcb8f799fc084a8b285932c22d592ffbcbd419d3
Katie Vermazani <katievermazani@tuta.io>
2022-02-12 18:56:49 -0700

    fixed classname and getting values from form

--- client/src/components/OpportunityEdit.jsx
+++ client/src/components/OpportunityEdit.jsx
@@ -35,19 +35,20 @@ export default function OpportunityEdit() {
     }, [])
 
     const handleSubmit = async (e) => {
+        console.log('submitting', e.target)
         e.preventDefault()
         const res = await Client.put(`/api/opportunities/${id}`,
             {
-                jobTitle: e.target.jobTitle.value,
-                stage: e.target.stage.value,
-                company: e.target.company.value,
-                payRange: e.target.payRange.value,
-                location: e.target.location.value,
-                pointOfContact: e.target.jobTitle.value,
-                phoneNumber: e.target.phoneNumber.value,
-                jobPostURL: e.target.jobPostURL.value,
-                companyURL: e.target.companyURL.value,
-                dateApplied: e.target.dateApplied.value,
+                jobTitle: document.getElementById('jobTitle').value,
+                stage: document.getElementById('stage').value,
+                company: document.getElementById('company').value,
+                payRange: document.getElementById('payRange').value,
+                location: document.getElementById('location').value,
+                pointOfContact: document.getElementById('pointOfContact').value,
+                phoneNumber: document.getElementById('phoneNumber').value,
+                jobPostURL: document.getElementById('jobPostURL').value,
+                companyURL: document.getElementById('companyURL').value,
+                dateApplied: document.getElementById('dateApplied').value,
                 // notes
             }).then(() => {
                 alert('Your opportunity has successfully been recorded!')
@@ -71,121 +72,137 @@ export default function OpportunityEdit() {
             <form onSubmit={handleSubmit}>
                 <div className='input-container'>
                     <div className='row-1'>
-                        <div classname="label-input-box">
-                            <label for="jobTitle">Job Title</label>
+                        <div className="label-input-box">
+                            <label htmlFor="jobTitle">Job Title</label>
                             <input
                                 className='edit-input'
                                 type="text"
                                 defaultValue={opportunity.jobTitle}
                                 name="jobTitle"
+                                id='jobTitle'
                             />
                         </div>
-                        <div classname="label-input-box">
-                            <label for="company">Company</label>
+                        <div className="label-input-box">
+                            <label htmlFor="company">Company</label>
                             <input
                                 className='edit-input'
                                 type="text"
                                 defaultValue={opportunity.company}
                                 name="company"
+                                id='company'
                             />
                         </div>
-                        <div classname="label-input-box">
-                            <label for='payRange'>Pay Range</label>
+                        <div className="label-input-box">
+                            <label htmlFor='payRange'>Pay Range</label>
                             <input
                                 className='edit-input'
                                 type="text"
                                 defaultValue={opportunity.payRange}
                                 placeholder="Pay Range"
                                 name="payRange"
+                                id='payRange'
                             />
                         </div>
                     </div>
                     <div className='row-2'>
-                        <div classname="label-input-box">
+                        <div className="label-input-box">
                             <label>Location</label>
                             <input
                                 className='edit-input'
                                 type="text"
                                 defaultValue={opportunity.location}
                                 name="location"
+                                id='location'
                             />
                         </div>
-                        <div classname="label-input-box">
+                        <div className="label-input-box">
                             <label>Point Of Contact</label>
                             <input
                                 className='edit-input'
                                 type="text"
                                 defaultValue={opportunity.pointOfContact}
-                                name="point-of-contact"
+                                name="pointOfContact"
+                                id="pointOfContact"
                             />
                         </div>
-                        <div classname="label-input-box">
+                        <div className="label-input-box">
                             <label>Phone Number</label>
                             <input
                                 className='edit-input'
                                 type="text"
                                 defaultValue={opportunity.phoneNumber}
-                                name="phone-number"
+                                name="phoneNumber"
+                                id="phoneNumber"
                             />
                         </div>
                     </div>
                     <div className='row-3'>
-                        <div classname="label-input-box">
+                        <div className="label-input-box">
                             <label>Job Post URL</label>
                             <input
                                 className='edit-input'
                                 type="text"
                                 defaultValue={opportunity.jobPostURL}
-                                name="job-post-url"
+                                name="jobPostURL"
+                                id='jobPostURL'
                             />
                         </div>
-                        <div classname="label-input-box">
+                        <div className="label-input-box">
                             <label>Company URL</label>
                             <input
                                 className='edit-input'
                                 type="text"
                                 defaultValue={opportunity.companyURL}
-                                name="company-url"
+                                name="companyURL"
+                                id="companyURL"
                             />
                         </div>
-                        <div classname="label-input-box">
+                        <div className="label-input-box">
                             <label>Date Applied</label>
                             <input
                                 className='edit-input'
                                 type="text"
                                 defaultValue={opportunity.dateApplied}
-                                name="date-applied"
+                                name="dateApplied"
+                                id="dateApplied"
                             />
                         </div>
                     </div>
                 </div>
                 <div className='notes-and-buttons'>
-                    <label for="notes">Notes</label>
+                    <div className='notes-label'>
+                        <label htmlFor="notes">Notes</label>
+                    </div>
                     <input
                         className='note-input'
                         type="text"
                         defaultValue={opportunity.notes}
                         placeholder='Notes'
                         name="notes"
+                        id='notes'
                     />
                     <div className='button-box'>
-                        <div classname="label-input-box">
-                            <label for="stage">Stage</label>
-                            <select
-                                className='edit-input'
-                                value={opportunity.stage}
-                                name="stage"
-                            >
-                                <option value="-">-</option>
-                                <option value="wishlist">wishlist</option>
-                                <option value="applied">applied</option>
-                                <option value="interview">interview</option>
-                                <option value="offer">offer</option>
-                                <option value="rejected">rejected</option>
-                            </select>
+                        <div className="stage-label-and-dropdown">
+                            <label htmlFor="stage">Stage
+                                <select
+                                    className='edit-input'
+                                    value={opportunity.stage}
+                                    name="stage"
+                                    id="stage"
+                                >
+                                    <option value="-">-</option>
+                                    <option value="wishlist">wishlist</option>
+                                    <option value="applied">applied</option>
+                                    <option value="interview">interview</option>
+                                    <option value="offer">offer</option>
+                                    <option value="rejected">rejected</option>
+                                </select>
+                            </label>
                         </div>
-                        <button type="submit" >Update Opportunity</button>
-                        <button onClick={handleDelete}>Delete Opportunity</button>
+                        <div className='update-and-delete-buttons'>
+                            <button className='these-buttons' type="submit" >Update Opportunity</button>
+                            <button className='these-buttons' onClick={handleDelete}>Delete Opportunity</button>
+                        </div>
                     </div>
                 </div>
             </form>
