5816eb19ed9201ab773263e4d412799e3ae4d89b
Katie Vermazani <katievermazani@tuta.io>
2022-02-13 10:59:21 -0700

    landing page css is done

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -7,15 +7,13 @@ const LandingPage = () => {
     return (
         <div className='landing-box'>
             <div className='description'>
-                <h2>A managment system for your job hunt</h2>
-                <h4>Organize all of your job opportunities in one place</h4>
+                <h2 className="welcome-line-1">A managment system for your job hunt</h2>
+                <h4 className="welcome-line-2">Organize all of your job opportunities in one place</h4>
                 <Link className='sign-up' to="/newaccount">Sign up for free</Link>
-            </div>
-            <div className='image'>
-                <img src="https://imgur.com/UvfqMGk"></img>
             </div>
         </div>
     )
 }
+
 
 export default LandingPage
