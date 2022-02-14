ad81e3c44ae58bdc23dde497ca07f0f4f9e4e4f0
Katie Vermazani <katievermazani@tuta.io>
2022-02-14 08:04:07 -0700

    fixed name of interview component

---
+++ client/src/components/Interview.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Client from '../services/api'
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Interview(authUser, ...props) {
+
+    const [interviews, setInterviews] = useState([])
+
+    // const navigate = useNavigate()
+
+    useEffect(() => {
+        Client.get(`/api/interviews`)
+            .then(interviews => {
+                setInterviews(interviews.data)
+            })
+    }, [authUser, props.authenticated])
+
+    console.log(interviews)
+
+    return (
+        <div className="display-interview-box">
+            {interviews && interviews.map((interview) => (
+                <div key={interview.id} className='individual-interview'>
+                    <p>Interviewer: {interview.interviewer}</p>
+                    <p>Time: {interview.time}</p>
+                    <p> Date: {interview.date}</p>
+                    <p>Meeting Link:{interview.meetingLink}</p>
+                    <p>Location: {interview.location}</p>
+                    <p>Notes: {interview.notes}</p>
+
+                    <Link to={(`/interviews/${interview.id}`)} className="details-link">Details</Link>
+                </div>
+            ))}
+        </div>
+    )
+}
